1c5b4fe4f0f621e04a557a765864979bb2da2a2c
klporter1219 <78804731+klporter1219@users.noreply.github.com>
2021-10-31 22:32:30 -0400

    Added working signup

--- app/app.js
+++ app/app.js
@@ -5,9 +5,55 @@ function route() {
     MODEL.getPages(id);
 }
 
+function setAuthed(authed) {
+    if (!authed) {
+        $.get(`./nav/unauthed.html`, function (data) {
+            $("#nav").html(data);
+        });
+        $.get(`./footer/unauthed.html`, function (data) {
+            $("#footer").html(data);
+        });
+    } else {
+        $.get(`./nav/authed.html`, function (data) {
+            $("#nav").html(data);
+        });
+        $.get(`./footer/authed.html`, function (data) {
+            $("#footer").html(data);
+        });
+    }
+}
+
 function initSite() {
     $(window).on('hashchange', route);
     route();
+    setAuthed(false);
+}
+
+function createAccount() {
+    const inputs = $('#signUp :input');
+    const values = {};
+    inputs.each(function() {
+        values[this.name] = $(this).val();
+    });
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(values.email, values.password)
+    .then((userCredential) => {
+    setAuthed(true);
+
+    inputs.each(function() {
+        $(this).val('');
+    });
+    })
+    .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    // ..
+    });
+
+    return false;
 }
 
 $(document).ready(function (){
--- app/model.js
+++ app/model.js
@@ -1,7 +1,7 @@
 var MODEL = (function () {
 
     var _getPages = function (pagesName) {
-        $.get(`./pages/${pagesName}/${pagesName}.html`, function (data){
+        $.get(`./pages/${pagesName}/${pagesName}.html?t=${Date.now()}`, function (data){
             $("#app").html(data);
         });
     };
